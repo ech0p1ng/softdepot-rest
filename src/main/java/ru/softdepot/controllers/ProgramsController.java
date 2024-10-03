2ab6cb8ee6d30ea35aa7ab4aa26d84fe3f8ed055
oselish <lesha2003petrov@gmail.com>
2024-10-03 12:38:02 +0500

    added controllers

---
+++ src/main/java/ru/softdepot/controllers/ProgramsController.java
@@ -0,0 +1,12 @@
+package ru.softdepot.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("softdepot-api/programs")
+@AllArgsConstructor
+public class ProgramsController {
+
+}
